d774d7d7f2b650cada4c190fce1ff7ebd9f6a642
Fhupsel <fmhupsel@outlook.com>
2024-04-22 09:19:17 -0300

    resp

---
+++ script.js
@@ -0,0 +1,64 @@
+alert('Hello World!')
+
+const num1 = 10
+const num2 = 15
+
+const soma = num1 + num2
+
+alert(soma)
+
+const number = 10
+
+if (typeof number == 'number') {
+  alert('É um número')
+} else {
+  alert('Não é um número')
+}
+
+const string = 'Exemplo de texto'
+
+if (typeof string == 'string') {
+  alert('É uma string')
+} else {
+  alert('Não é uma string')
+}
+
+const bool = true
+
+if (typeof bool == 'boolean') {
+  alert('É um booleano')
+} else {
+  alert('Não é um booleano')
+}
+
+const sub = num1 - num2
+
+alert(sub)
+
+
+const mult = num1 * num2
+
+alert(mult)
+
+
+const div = num1 / num2
+
+alert(div)
+
+
+const numPar = 4
+
+if (numPar % 2 == 0) {
+  alert('É um número par')
+} else {
+  alert('Não é um número par')
+}
+
+
+const numImpar = 5
+
+if (numImpar % 2 != 0) {
+  alert('É um número ímpar')
+} else {
+  alert('Não é um número ímpar')
+}
